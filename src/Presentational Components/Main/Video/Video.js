de54553c9d84c5684bb66363e81b338f8d730ec9
Fandix <fandix0925@gmail.com>
2020-07-10 17:39:36 +0800

    update 2020/07/10 Create Video Length but the UI of Second is not Finish

--- src/Presentational Components/Main/Video/Video.js
+++ src/Presentational Components/Main/Video/Video.js
@@ -4,13 +4,21 @@ import style from "./Video.module.scss"
 const Video = (props) => {
     console.log(props)
     const { Video } = props;
+
+    const VideoLength = (duration) => {
+        const Reg = new RegExp('[0-9]+[A-Z][0-9]+');
+        const tmp = duration.match(Reg);
+        const Result = tmp[0].replace('M',":");
+        return Result
+      }
+
     return(
         <div className={style.wrap}>
             {/*********** img ***********/}
             <div className={style.img}>
                 <img src={Video.snippet.thumbnails.medium.url} alt="Cover" />
                 <div className={style.length}>
-                    {/* <p>{Video.contentDetails.}</p> */}
+                    <p>{VideoLength(Video.contentDetails.duration)}</p>
                 </div>
             </div>
             {/*********** Title ***********/}
