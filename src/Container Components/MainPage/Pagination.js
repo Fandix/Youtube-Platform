5021d314b98e07df23648bdcf6b45b30d0ad0e3f
Fandix Huang <fandix0925@gamil.com>
2020-07-12 21:49:00 +0800

    update 2020/07/12 New Function of RollToTop

--- src/Container Components/MainPage/Pagination.js
+++ src/Container Components/MainPage/Pagination.js
@@ -2,6 +2,27 @@ import { connect } from "react-redux";
 import Page from ".././../Presentational Components/Main/Pagination/Pagination"
 import { RequestPost,Fetch_Response } from "../../Redux/Action/MainAction";
 
+const ScrollTop = (number = 0,time) => {
+    if(!time){
+        document.body.scrollTop = document.documentElement.scrollTop = number;
+        return number;
+    }
+
+    const spacingTime = 20;
+    let spacingInex = time / spacingTime;
+    let nowTop = document.body.scrollTop + document.documentElement.scrollTop;
+    let everTop = (number - nowTop) / spacingInex;
+
+    let scrollTimer = setInterval(() => {
+        if (spacingInex > 0) {
+            spacingInex--;
+            this.ScrollTop(nowTop += everTop);
+        } else {
+            clearInterval(scrollTimer); 
+        }
+    }, spacingTime);
+};
+
 const mapStateToProps = (state) => {
     return{
         next:state.FetchResponseReducer.next,
@@ -14,10 +35,12 @@ const mapDispatchToProps = (dispatch) => {
         onNextClick:(next) => {
             dispatch(RequestPost());
             dispatch(Fetch_Response(next));
+            ScrollTop();
         },
         onPrevClick:(prev) => {
             dispatch(RequestPost());
             dispatch(Fetch_Response(prev));
+            ScrollTop();
         }
     }
 };
